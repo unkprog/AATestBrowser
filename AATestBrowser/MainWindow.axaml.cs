edc4288d54dd206cb0139782295527b68ba3f9a4
unkprog <unkprog@gmail.com>
2022-09-13 23:13:58 +0300

    Добавьте файлы проекта.

---
+++ AATestBrowser/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace AATestBrowser
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
